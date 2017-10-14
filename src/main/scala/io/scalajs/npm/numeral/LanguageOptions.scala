1aa4191c4bd8154c21eab6108b407f34b554a092
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2017-10-14 12:07:12 -0700

    Release v0.4.2

--- src/main/scala/io/scalajs/npm/numeral/LanguageOptions.scala
+++ src/main/scala/io/scalajs/npm/numeral/LanguageOptions.scala
@@ -1,13 +1,13 @@
 package io.scalajs.npm.numeral
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.ScalaJSDefined
+
 
 /**
   * Numeral Language Options
   * @author lawrence.daniels@gmail.com
   */
-@ScalaJSDefined
+
 class LanguageOptions(var delimiters: js.UndefOr[js.Dictionary[String]] = js.undefined,
                       var abbreviations: js.UndefOr[js.Dictionary[String]] = js.undefined,
                       var ordinal: js.Function1[Int, String] = null,
